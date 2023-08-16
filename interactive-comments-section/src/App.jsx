0797722a77e8af3a6afe9b55720074452311f51c
K3nK0 <uncommonmen@hotmail.fr>
2023-08-16 22:48:28 +0200

    partie form dans l'app.jsx

--- interactive-comments-section/src/App.jsx
+++ interactive-comments-section/src/App.jsx
@@ -1,39 +1,75 @@
 import './App.css'
 import { useEffect, useState } from 'react'
-import Comments from './components/comments/Comments'
-import FormComment from './components/FormComment/FormComment'  
+import { nanoid } from "nanoid"
+import Comments from './components/comments/Comments' 
 
 function App() {
 
   const [currentUser, setCurrentUSer] = useState({
     id: "currentUser",
     name: undefined,
-    profile: undefined
+    photoProfile: undefined
   })
 
-  const [comment, setComment] = useState({
-    id: 1,
+  const [comments, setComments] = useState([
+    {id: 1,
     content: "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
     createdAt: "1 month ago",
     score: 12,
     userImg: "./public/avatars/image-amyrobson.webp",
     username: "amyrobson",
-    replies: {}
-  })
+    replies: {}},
+    ])
 
   useEffect(() => {
     fetch("./data.json")
     .then(res => res.json())
     .then(data => {
-      setCurrentUSer({name: data.currentUser.username, profile: data.currentUser.image.webp})
+      setCurrentUSer({name: data.currentUser.username, photoProfile: data.currentUser.image.webp})
     })
 }, [])
+
+  const [newComment, setNewComment] = useState("")
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    if(newComment === ""){
+      return
+    }
+
+    setNewComment("")
+
+    setComments([...comments, {
+      id: nanoid(8),
+      content: newComment,
+      createdAt: "just now",
+      score: 0,
+      userImg: currentUser.photoProfile,
+      username: "juliusomo",
+    }])
+  }
+
+  console.log(comments);
 
   return (
     <>
     <div className="container">
-      <Comments comment={comment}/>
-      <FormComment profile={currentUser.profile} />
+      {comments.length > 0 &&
+      comments.map(com => (
+        <Comments key={com.id} comment={com} />
+      ))}
+      
+      <form onSubmit={handleSubmit}>
+        <img src={currentUser.photoProfile} alt="photo profile" />
+        <textarea 
+        value={newComment}
+        onChange={e => setNewComment(e.target.value)}
+        placeholder="Add a comment..."/>
+        <button className="btn-send">SEND</button>
+      </form> 
+
+      {/* <FormComment profile={currentUser.profile} setComments={setComments} comments={comments} /> */}
     </div>
     </>
   )
