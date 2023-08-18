77f96e77f50d40f0ce132eb75d2e6cda6e1738d6
K3nK0 <uncommonmen@hotmail.fr>
2023-08-18 12:30:43 +0200

    création container replies

--- interactive-comments-section/src/App.jsx
+++ interactive-comments-section/src/App.jsx
@@ -18,7 +18,25 @@ function App() {
     score: 12,
     userImg: "./public/avatars/image-amyrobson.webp",
     username: "amyrobson",
-    replies: {}},
+    replies: [{
+      id: 3,
+      content: "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+      createdAt: "1 week ago",
+      score: 4,
+      replyingTo: "maxblagun",
+      userImg: "./public/avatars/image-ramsesmiron.webp",
+      username: "ramsesmiron"
+      },
+      {
+      id: 4,
+      content: "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+      createdAt: "2 days ago",
+      score: 2,
+      replyingTo: "ramsesmiron",
+      userImg: "./public/avatars/image-juliusomo.png",
+      username: "juliusomo"
+      }]
+      },
     ])
 
   useEffect(() => {
@@ -54,12 +72,16 @@ function App() {
     setComments(comments.filter(comment => comment.id != id))
   }
 
+  function deleteReplie(id){
+    setComments(comments.replies.filter(replie => replie.id != id))
+  }
+
   return (
     <>
     <div className="container">
       {comments.length > 0 &&
       comments.map(com => (
-        <Comments key={com.id} comment={com} currentUser={currentUser.name} deleteComment={deleteComment} />
+        <Comments key={com.id} comment={com} currentUser={currentUser.name} deleteComment={deleteComment} deleteReplie={deleteReplie} />
       ))}
       
       <form onSubmit={handleSubmit}>
